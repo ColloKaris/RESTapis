c72a4636df94576583ea9534bf5d2932ab895947
Collins Kariuki <collinsmunkary@gmail.com>
2023-12-31 08:23:44 +0300

    Chaining requests

--- tomDoesTech/src/app.ts
+++ tomDoesTech/src/app.ts
@@ -5,20 +5,20 @@ const app = express();
 // parse JSON bodies
 app.use(express.json());
 
-app.get('/',(req: Request, res: Response) => {
-  res.redirect('http://example.com')
+app.route('/')
+.get((req: Request, res: Response) => {
+  return res.send('You made a GET request');
+})
+.post((req: Request, res: Response) => {
+  res.send("You made a POST request");
+})
+.put((req: Request, res: Response) => {
+  res.send("You made a PUT request");
+})
+.delete((req: Request, res: Response) => {
+  console.log("You made an DELETE request")
 })
 
-app.post('/api/data', (req: Request, res:Response) => {
-  console.log(req.body);
-
-  return res.sendStatus(200);
-})
-
-// Listens to all http methods
-app.all('/api/all', (req:Request, res:Response) => {
-  return res.sendStatus(200);
-})
 
 
 
